0f1297e198c0b84bb49e0641733640f32b1cb628
Carlos Duron <carlosduron973@gmail.com>
2023-03-01 11:28:16 -0600

    registar vheiculos

--- src/app/components/personas/personas.component.ts
+++ src/app/components/personas/personas.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild,OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { PersonaService } from 'src/app/services/Personas/persona.service';
@@ -8,6 +8,10 @@ import { Route, Router } from '@angular/router';
 import { LoginService } from 'src/app/services/Login/log.service';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BikeM } from 'src/app/Models/biker.model';
+import { BikerService } from 'src/app/services/Bikers/bike.service';
+import { CarM } from 'src/app/Models/car.model';
+import { CarService } from 'src/app/services/Cars/car.service';
 
 @Component({
     selector: 'app-personas',
@@ -21,6 +25,16 @@ export class PersonasComponent {
     @ViewChild('contenido') contenido: any;
     @ViewChild('eliminar') eliminar: any;
 
+
+    bikes: BikeM[] = [];
+    idB?: number;
+    brand: string = '';
+    model: string = '';
+    year: string = '';
+    color: string = '';
+
+    cars: CarM[] = [];
+
     personas: PersonaM[] = [];
     id: number = 0;
     name: string = '';
@@ -28,80 +42,83 @@ export class PersonasComponent {
     role: string = '';
     active: string = '';
     constructor(private modalService: NgbModal, private personaService: PersonaService, private fb: FormBuilder,
-        private location: Location,private router: Router,private authService: LoginService) {
-    
+        private location: Location, private router: Router, private authService: LoginService, private bikeService: BikerService, private carservice:CarService) {
+
     }
 
     ngOnInit(): void {
         const token = localStorage.getItem('token') ?? '';
         console.log(token);
 
-    // Or 'admin', depending on the role of the user
+        // Or 'admin', depending on the role of the user
         this.personaService.getPersonas(token).subscribe((res) => {
             this.personas = res;
             console.log(res)
-            
+
         });
-        } 
+    }
 
 
 
-        cerrarSesion() {
-            this.authService.logout();
-            this.router.navigate(['/inicio']);
-          }
+    cerrarSesion() {
+        this.authService.logout();
+        this.router.navigate(['/inicio']);
+    }
 
 
-   
+
 
 
     abrirModal(persona: any) {
         this.modalService.open(this.contenido, { centered: true });
         this.id = persona.id;
-        console.log("id",this.id);
-       
-      }
-    abrirC()
-    {
-       const token = localStorage.getItem('token') ?? '';
-       const id = this.id;
-       const form = {
+        console.log("id", this.id);
+
+    }
+    abrirC() {
+        const token = localStorage.getItem('token') ?? '';
+        const id = this.id;
+        const form = {
             name: this.name,
             phone: this.phone,
             role: JSON.parse(this.role),
-           active: JSON.parse(this.active)
-            } 
+            active: JSON.parse(this.active)
+        }
 
-        this.personaService.updatePersona(form,id,token).subscribe(
+        this.personaService.updatePersona(form, id, token).subscribe(
             (res) => {
                 this.ngOnInit();
                 alert("Successful");
             },
             (err) => {
-                if (err.status == 500)
-                {
-                  alert("Nos encontramos en mantenimiento");
+                if (err.status == 500) {
+                    alert("Nos encontramos en mantenimiento");
                 }
-                
+
 
             }
         );
 
-            console.log("formulario",form);
-            console.log("id",id);
-        
-            
-           }
+        console.log("formulario", form);
+        console.log("id", id);
 
-    EliminarP( persona: any) {
-        
+
+    }
+
+    EliminarP(persona: any) {
+
         this.id = persona.id;
-        console.log("id",this.id);
+        console.log("id", this.id);
         const token = localStorage.getItem('token') ?? '';
-        this.personaService.deletePersona(this.id,token).subscribe(
+        this.personaService.deletePersona(this.id, token).subscribe(
             (res) => {
                 alert("Successful");
             }
         );
+    }
+
+    openModal(template: any, persona: any) {
+        const modalRef = this.modalService.open(template);
+        modalRef.componentInstance.persona = persona;
     }
 }
