fa1c27481cbfb02aed0a5bdaacb05769cc37ba7a
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-03-02 00:40:23 -0600

    ;( NS

--- src/app/components/eliminarcar/eliminarcar.component.ts
+++ src/app/components/eliminarcar/eliminarcar.component.ts
@@ -7,6 +7,7 @@ import { CarM } from 'src/app/Models/car.model';
 import { BikeM } from 'src/app/Models/biker.model';
 import { BikerService } from 'src/app/services/Bikers/bike.service';
 import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-eliminarcar',
@@ -16,39 +17,47 @@ import { Location } from '@angular/common';
 })
 export class EliminarcarComponent {
 
-  cars: CarM[]=[]
-  bikes:BikeM[]=[]
+  cars: any[]=[]
+  bikes:any[]=[]
+  bikess:BikeM[]=[]
+  carss:CarM[]=[]
+  id: number = 0;
   items = ['item 1'+ 1];
 
 
   constructor(private modal: NgbModal, private carService: CarService,
-    private location: Location,private router: Router,private bikeService:BikerService) { }
+    private location: Location,private router: Router,private bikeService:BikerService, private activeRoute:ActivatedRoute) { }
 
     ngOnInit(): void {
+      this.activeRoute.queryParams.subscribe(params => {
+        const id = params['id'];
+        console.log("ID",id); 
+        this.id = id;
+      });
       const token = localStorage.getItem('token') ?? '';
       console.log(token);
     
     // Or 'admin', depending on the role of the user
       this.carService.getCars(token).subscribe((res) => {
-          this.cars = res;
+          this.carss = res;
           console.log(res);
-      },
-      (err) => {
-        if (err == 500)
-        {
-          alert("Nos encontramos en mantenimiento");
-        }
-      });
-      this.bikeService.getBikes(token).subscribe((res) => {
-        this.bikes = res;
-        console.log(res);
-    },
-    (err) => {
-      if (err == 500)
-      {
-        alert("Nos encontramos en mantenimiento");
       }
+      );
+      this.bikeService.getBikes(token).subscribe((res) => {
+        this.bikess = res;
+        console.log(res);
+    }
+    );
+    this.bikeService.BikeUser(token,this.id).subscribe((res) => {
+      this.bikes = res;
+      console.log(res);
     });
+    this.carService.CarUser(token,this.id).subscribe((res) => {
+      this.cars = res;
+      console.log(res);
+    });
+
+
     
     }
 
